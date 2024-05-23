47ea3b292e2b72a956d619c6fb6df1a8aba0c771
Rogger Salazar <102567758+roggergolden@users.noreply.github.com>
2024-05-23 09:26:59 -0500

    Add files via upload

---
+++ vienesweb - aa2/js/header.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const nav = document.querySelector('.nav');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        nav.classList.toggle('active');
+    });
+});
